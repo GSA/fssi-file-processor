e352bab25f7b922d1b45fb8ed885bf0fa93bb565
David Larrimore <davidlarrimore@gmail.com>
2014-12-19 05:55:36 -0500

    Adding Base File Structure and Libraries
    CSV, Email, and IO from Apache Commons

---
+++ src/gov/gsa/fssi/Main.java
@@ -0,0 +1,17 @@
+package gov.gsa.fssi;
+
+/**
+ * This is the main class for the FSSI File Processor Project
+ * 
+ * @author David Larrimore
+ * @version 0.1
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+	
+}
