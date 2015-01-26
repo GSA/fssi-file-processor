978dae427189844f5ba897cb3d8473091f06a202
David Larrimore <davidlarrimore@gmail.com>
2015-01-25 20:55:58 -0500

    Date Type Validation Added

--- src/gov/gsa/fssi/files/sourceFiles/utils/strategies/typeValidations/DateTypeValidationStrategy.java
+++ src/gov/gsa/fssi/files/sourceFiles/utils/strategies/typeValidations/DateTypeValidationStrategy.java
@@ -4,7 +4,7 @@ import gov.gsa.fssi.files.schemas.schemaFields.SchemaField;
 import gov.gsa.fssi.files.sourceFiles.records.datas.Data;
 import gov.gsa.fssi.files.sourceFiles.utils.strategies.TypeValidationStrategy;
 
-public class StringTypeValidationStrategy implements TypeValidationStrategy {
+public class DateTypeValidationStrategy implements TypeValidationStrategy {
 
 	@Override
 	public void validate(SchemaField field, Data data) {
