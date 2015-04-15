80634699bc52d6fd1cb5833903ccbd5dbbaa0ae3
David Larrimore <david.larrimore@gsa.gov>
2015-04-15 15:11:44 -0400

    Updated for 1.4-SNAPSHOT

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 
 ECHO Executing FSSI File Processor
-java -jar -Xms512m -Xmx2056m fssi-file-processor-1.3-SNAPSHOT-eba3ce6-jar-with-dependencies.jar
+java -jar -Xms512m -Xmx2056m fssi-file-processor-1.3-SNAPSHOT-d38f68b-jar-with-dependencies.jar
 ECHO Completed
 Pause
